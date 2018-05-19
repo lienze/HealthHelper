3f18bb90d2de544c7c8ef385ccbe9883323eab51
lienze <lienze2010@126.com>
2018-05-19 16:07:21 +0800

    修改入口函数

--- HealthHelper/main.cpp
+++ HealthHelper/main.cpp
@@ -1,12 +1,18 @@
 #include "widget.h"
+#include "mainwidget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
+
+    //首先显示主界面，进行相关设置
+    MainWidget mainWidget;
+    mainWidget.show();
+
+    //Widget w;
     //w.show();
     //全屏显示
-    w.showFullScreen();
+    //w.showFullScreen();
     return a.exec();
 }
