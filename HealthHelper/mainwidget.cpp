88b1d027cfe83f42dea7c9a9c4f81d4fa13a56d3
lienze <lienze2010@126.com>
2018-06-30 19:10:30 +0800

    完善托盘图标逻辑

--- HealthHelper/mainwidget.cpp
+++ HealthHelper/mainwidget.cpp
@@ -8,6 +8,7 @@ MainWidget::MainWidget(QWidget *parent) :
 {
     ui->setupUi(this);
     mSysTrayIcon = NULL;
+
 }
 
 MainWidget::~MainWidget()
@@ -26,16 +27,23 @@ void MainWidget::on_pushButton_confirm_clicked()
 
     //隐藏程序主窗口
     this->hide();
-    //新建QSystemTrayIcon对象
-    mSysTrayIcon = new QSystemTrayIcon(this);
-    //新建托盘要显示的icon
-    QIcon icon = QIcon(":/images/desktop_icon.png");
-    //将icon设到QSystemTrayIcon对象中
-    mSysTrayIcon->setIcon(icon);
-    //当鼠标移动到托盘上的图标时，会显示此处设置的内容
-    mSysTrayIcon->setToolTip("HealthHelper");
-    //给QSystemTrayIcon添加槽函数
-    connect(mSysTrayIcon,SIGNAL(activated(QSystemTrayIcon::ActivationReason)),this,SLOT(on_activatedSysTrayIcon(QSystemTrayIcon::ActivationReason)));
+    //初始化QSystemTrayIcon对象
+    if(mSysTrayIcon==NULL)
+    {
+        mSysTrayIcon = new QSystemTrayIcon(this);
+        //新建托盘要显示的icon
+        QIcon icon = QIcon(":/images/desktop_icon.png");
+        //将icon设到QSystemTrayIcon对象中
+        mSysTrayIcon->setIcon(icon);
+        //当鼠标移动到托盘上的图标时，会显示此处设置的内容
+        mSysTrayIcon->setToolTip("HealthHelper");
+        //给QSystemTrayIcon添加槽函数
+        connect(mSysTrayIcon,SIGNAL(activated(QSystemTrayIcon::ActivationReason)),this,SLOT(on_activatedSysTrayIcon(QSystemTrayIcon::ActivationReason)));
+
+        //建立托盘操作的菜单
+        createActions();
+        createMenu();
+    }
     //在系统托盘显示此对象
     mSysTrayIcon->show();
 }
@@ -45,6 +53,15 @@ void MainWidget::on_activatedSysTrayIcon(QSystemTrayIcon::ActivationReason reaso
     switch(reason){
     case QSystemTrayIcon::Trigger:
         //单击托盘图标
+        //显示消息球，1s后自动消失
+        //第一个参数是标题
+        //第二个参数是消息内容
+        //第三个参数图标
+        //第四个参数是超时毫秒数
+        mSysTrayIcon->showMessage(tr("Message Title"),
+                                  tr("欢迎使用此程序"),
+                                  QSystemTrayIcon::Information,
+                                  1000);
         break;
     case QSystemTrayIcon::DoubleClick:
         //双击托盘图标
@@ -54,4 +71,36 @@ void MainWidget::on_activatedSysTrayIcon(QSystemTrayIcon::ActivationReason reaso
     default:
         break;
     }
+}
+
+void MainWidget::createActions()
+{
+    mShowMainAction = new QAction(tr("显示主界面"),this);
+    connect(mShowMainAction,SIGNAL(triggered()),this,SLOT(on_showMainAction()));
+
+    mExitAppAction = new QAction(tr("退出"),this);
+    connect(mExitAppAction,SIGNAL(triggered()),this,SLOT(on_exitAppAction()));
+
+}
+
+void MainWidget::createMenu()
+{
+    mMenu = new QMenu(this);
+    mMenu->addAction(mShowMainAction);
+
+    mMenu->addSeparator();
+
+    mMenu->addAction(mExitAppAction);
+
+    mSysTrayIcon->setContextMenu(mMenu);
+}
+
+void MainWidget::on_showMainAction()
+{
+    this->show();
+}
+
+void MainWidget::on_exitAppAction()
+{
+    exit(0);
 }
--- HealthHelper/mainwidget.h
+++ HealthHelper/mainwidget.h
@@ -3,6 +3,8 @@
 
 #include <QWidget>
 #include <QSystemTrayIcon>
+#include <QMenu>
+#include <QAction>
 
 namespace Ui {
 class MainWidget;
@@ -16,11 +18,19 @@ public:
     explicit MainWidget(QWidget *parent = 0);
     ~MainWidget();
     QSystemTrayIcon *mSysTrayIcon;
+    void createActions();
+    void createMenu();
+    QMenu *mMenu;
+    QAction *mShowMainAction;
+    QAction *mExitAppAction;
 
 private slots:
     void on_pushButton_cancle_clicked();
     void on_pushButton_confirm_clicked();
     void on_activatedSysTrayIcon(QSystemTrayIcon::ActivationReason reason);
+
+    void on_showMainAction();
+    void on_exitAppAction();
 private:
     Ui::MainWidget *ui;
 };
